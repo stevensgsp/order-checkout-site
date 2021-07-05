255823eac02a7a6bcc812fea9d7ef4aeb5243d44
Steven Sucre <steven.g.s.p@gmail.com>
2021-07-05 13:20:04 -0400

    Readme update and minor changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,7 @@
 @extends('partials.app')
 
 @section('content')
-    Hola!
+    <span>Welcome to Order Checkout Site! A basic store, where a customer can only buy a single product with a fixed value.</span>
+    <br>
+    <a href="{{ route('orders.show-store') }}">Go to store</a>
 @endsection
